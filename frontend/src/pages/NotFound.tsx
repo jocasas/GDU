9f9f1cf2da898b3eaba9dbe8dec1bd1bb5c2f7bd
Jose Pablo Casas <jocasas@duocuc.cl>
2025-06-15 23:55:19 -0400

    Rebase Template

--- frontend/src/pages/NotFound.tsx
+++
@@ -1,6 +0,0 @@
-
-const NotFound = () => {
-  return <h1>404 - Página no encontrada</h1>;
-};
-
-export default NotFound;
--- frontend/src/pages/UsersPage.tsx
+++
@@ -1,6 +0,0 @@
-
-const UsersPage = () => {
-  return <h1>Página de Usuarios</h1>;
-};
-
-export default UsersPage;
